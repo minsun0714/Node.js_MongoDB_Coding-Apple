01e9f344b7b467c5e426328094ded5c16432b407
minsun0714 <woooooooow22@gmail.com>
2023-02-07 19:31:47 +0900

    🗃️ [과제] user에게 입력 받은 데이터 mongoDB에 저장

--- server.js
+++ server.js
@@ -4,9 +4,33 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const MongoClient = require("mongodb").MongoClient;
+var db;
 MongoClient.connect(
   "mongodb+srv://jasmine:dlalstjs22!@cluster0.aid8ftu.mongodb.net/?retryWrites=true&w=majority",
   function (error, client) {
+    if (error) return console.log(error);
+
+    db = client.db("todoapp");
+
+    app.post("/add", function (req, res) {
+      res.send("전송 완료!");
+      const toDo = req.body.title;
+      const date = req.body.date;
+      db.collection("post").insertOne(
+        { title: toDo, date: date, _id: Date.now() },
+        function (error, res) {
+          console.log("저장 완료 ~!~!~!!");
+        }
+      );
+    });
+
+    db.collection("post").insertOne(
+      { name: "John", age: 20, _id: 100 },
+      function (error, res) {
+        console.log("저장 완료 ~!~!~!!");
+      }
+    );
+
     app.listen(8080, function () {
       console.log("listening on 8080");
     });
@@ -27,10 +51,4 @@ app.get("/", function (req, res) {
 
 app.get("/write", function (req, res) {
   res.sendFile(__dirname + "/write.html");
-});
-
-app.post("/add", function (req, res) {
-  res.send("전송 완료!");
-  console.log(req.body.title);
-  console.log(req.body.date);
 });
