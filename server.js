d2032ab82315011000d5cb77cfa8fd5b41ef564c
minsun0714 <woooooooow22@gmail.com>
2023-02-09 19:59:13 +0900

    ✨delete 요청

--- server.js
+++ server.js
@@ -70,3 +70,13 @@ app.get("/", function (req, res) {
 app.get("/write", function (req, res) {
   res.sendFile(__dirname + "/write.html");
 });
+
+app.delete("/delete", function (req, response) {
+  req.body._id = parseInt(req.body._id);
+  db.collection("post").deleteOne(req.body, function (error, result) {
+    console.log("삭제 완료");
+    response.status(200).send({
+      message: "성공했슴당~",
+    });
+  });
+});
